615bf736b19cce3d7c76e2f41c09918964dda038
ElijahButers <Elijahbuters@gmail.com>
2017-02-14 14:11:27 +0200

    Added to the TestCoreDataStack class where this class subclasses CoreDataStack and only overrides the default value of a single property: storeContainer, and since we are overriding the value in init(), the persistent container from CoreDataStack isn’t used, and the persistent container in TestCoreDataStack uses an in-memory store only.

--- CampgroundManagerTests/TestCoreDataStack.swift
+++ CampgroundManagerTests/TestCoreDataStack.swift
@@ -6,4 +6,28 @@
 //  Copyright © 2017 Razeware. All rights reserved.
 //
 
+import CampgroundManager
 import Foundation
+import CoreData
+
+class TestCoreDataStack: CoreDataStack {
+  
+  override init(modelName: String) {
+    super.init(modelName: modelName)
+    
+    let persistentStoreDescription = NSPersistentStoreDescription()
+    persistentStoreDescription.type = NSInMemoryStoreType
+    
+    let container = NSPersistentContainer(name: modelName)
+    container.persistentStoreDescriptions = [persistentStoreDescription]
+    
+    container.loadPersistentStores {
+      (storeDescription, error) in
+      if let error = error as? NSError {
+        fatalError(
+          "Unresolved error \(error), \(error.userInfo)")
+      } }
+    self.storeContainer = container
+  }
+}
+
