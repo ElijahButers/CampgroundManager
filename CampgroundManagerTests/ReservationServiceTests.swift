7d0a6e26c36b902648f9a0e6244cb3ae50074ed0
ElijahButers <Elijahbuters@gmail.com>
2017-02-23 18:34:05 +0200

    Added code to the ReservationServiceTests class that has a slightly longer version of the set up and tear down code we’ve used in the previous test case classes, sets up the Core Data stack as usual, creates a fresh instance of each service in setUp for each test.

--- CampgroundManagerTests/ReservationServiceTests.swift
+++ CampgroundManagerTests/ReservationServiceTests.swift
@@ -6,30 +6,35 @@
 //  Copyright © 2017 Razeware. All rights reserved.
 //
 
+import Foundation
+import CoreData
 import XCTest
+import CampgroundManager
 
 class ReservationServiceTests: XCTestCase {
+  
+  var campSiteService: CampSiteService!
+  var camperService: CamperService!
+  var reservationService: ReservationService!
+  var coreDataStack: CoreDataStack!
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+      coreDataStack = TestCoreDataStack()
+      camperService = CamperService(managedObjectContext: coreDataStack.mainContext, coreDataStack: coreDataStack)
+      campSiteService = CampSiteService(managedObjectContext: coreDataStack.mainContext, coreDataStack: coreDataStack)
+      reservationService = ReservationService(managedObjectContext: coreDataStack.mainContext, coreDataStack: coreDataStack)
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+      
+      camperService = nil
+      campSiteService  = nil
+      reservationService = nil
+      coreDataStack = nil
     }
     
 }
